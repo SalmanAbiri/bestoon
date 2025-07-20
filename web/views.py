61a0a87672560c8fc7ac31daf5b2093b58c3ff24
SalmanAbiri <red.salman.abiri@gmail.com>
2025-07-20 03:33:35 +0000

    session 2.1 tokens

--- web/views.py
+++ web/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JasonResponse
+from utils.jason import JASONEncoder
 
 # Create your views here.
+def submit_expense(request):
+    """ user submit request an expense """
+    return JasonResponse({'status':'ok'}, encoder=JASONEncoder)
